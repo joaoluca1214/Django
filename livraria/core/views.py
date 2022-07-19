03c6e7499fc7c355018409c5439d206ad0cc1275
joaoluca1214 <joaoluca1214@gmail.com>
2022-07-19 06:14:01 -0300

    19/07/22 pt2

--- livraria/core/views.py
+++ livraria/core/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Autor, Categoria, Editora, Livro
-from core.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer
+from core.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer
 
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
@@ -17,4 +17,9 @@ class EditoraViewSet(ModelViewSet):
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
-    serializer_class = LivroSerializer
+    # serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list','retrieve']:
+            return LivroDetailSerializer
+        return LivroSerializer
